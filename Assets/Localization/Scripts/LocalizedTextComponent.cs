8a6cae752416de83225729f8a4146f7f38e40765
Harjot Singh <121034602+deadlockhook@users.noreply.github.com>
2024-11-18 10:23:40 -0400

    Dev Build for Localization

---
+++ Assets/Localization/Scripts/LocalizedTextComponent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class LocalizedTextComponent : MonoBehaviour
+{
+    [SerializeField] private string tableReference;
+    [SerializeField] private string localizationKey;
+
+    private LocalizedString localizedStr;
+    private Text textComponent;
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+        localizedStr = new LocalizedString { TableReference = tableReference, TableEntryReference = localizationKey };
+
+        //just for testing
+        //var frenchLocale = LocalizationSettings.AvailableLocales.GetLocale("fr");
+        //LocalizationSettings.SelectedLocale = frenchLocale;
+
+        LocalizationSettings.SelectedLocaleChanged += UpdateText;
+        UpdateText(LocalizationSettings.SelectedLocale);
+    }
+    void UpdateText(Locale locale)
+    {
+        textComponent.text = localizedStr.GetLocalizedString();
+    }
+}
